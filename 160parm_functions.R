a42aec86c32d268ab8e5f3b1bd6bc7415e68ef5c
DagHjermann <dag.hjermann@niva.no>
2021-11-18 15:03:43 +0000

    Add left_join2 function

--- 160parm_functions.R
+++ 160parm_functions.R
@@ -23,3 +23,45 @@ get_model_variables <- function(model){
 # Test
 # fit <- lm(sr ~ pop15*dpi + pop75*dpi + ddpi, data = LifeCycleSavings)
 # get_model_variables(fit)  
+
+
+#
+# left_join2 
+#
+# as 'left_join', but     
+# - Does not accept common variables that are not key variables (specified in "by")  
+# - Gives warning if there are nn-unique combinations of key variables i data set 2, so number of rows increase (duplicating rows in data set no. 1)   
+# - Optionally, Lists existing variables, key variables and added variables (if print_vars = TRUE)  
+
+left_join2 <- function(data1, data2, by = by, ..., print_vars = FALSE){
+  
+  common_vars <- intersect(names(data1), names(data2))
+  common_vars_not_in_by <- common_vars[!common_vars %in% by]
+  
+  if (length(common_vars_not_in_by) > 0){
+    stop("Columns ", paste(sQuote(common_vars_not_in_by), collapse = ", "), " exists in both data sets")
+  }
+  result <- left_join(data1, data2, by = by, ...)
+  
+  if (nrow(result) > nrow(data1)){
+    warning("Data set 2 does not have unique rows. Number of rows increased from ", 
+            nrow(data1), " to ", nrow(result))
+  }
+  
+  vars_added <-   names(data2)[!names(data2) %in% names(data1)]
+  
+  if (print_vars){
+    cat("Variables before join: \n")
+    paste(sQuote(names(data1), q = FALSE), collapse = ", ") %>% cat()
+    cat("\n\nVariables used to join: \n")
+    paste(sQuote(by, q = FALSE), collapse = ", ") %>% cat()
+    cat("\n\nVariables added: \n")
+    paste(sQuote(vars_added, q = FALSE), collapse = ", ") %>% cat()
+    cat("\n")
+    
+  }
+  
+  result
+  
+}
+
