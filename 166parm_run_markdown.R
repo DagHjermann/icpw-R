5217f31ad6631b5acd1ba62e41f5ebbfb0de5590
DagHjermann <dag.hjermann@niva.no>
2022-03-01 08:58:49 +0000

    Remove altitude, 166

--- 166parm_run_markdown.R
+++ 166parm_run_markdown.R
@@ -8,6 +8,11 @@
 #
 
 
+#
+# NOTE: form / logistic_formula can be ignored, it is not used in the script 
+#   (runs only random forest, not the logistic regression)
+#
+
 #o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
 #
 # 166a1 - All variables incl. catchment_area + TOC (coniferous + decid_mixed separately) ----
@@ -21,7 +26,7 @@
 vars <- paste(
   "slope_no3_vs_time", 
   "catchment_area, TOC",     # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, coniferous, decid_mixed, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -62,7 +67,7 @@ rmarkdown::render(
 vars <- paste(
   "slope_no3_vs_time", 
   "catchment_area, TOC",     # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, total_forest, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -102,8 +107,8 @@ rmarkdown::render(
 #   (or let each part end with a comma)
 vars <- paste(
   "slope_no3_vs_time", 
-  "TOC",                                # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "TOC, altitude",                                # change here
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, coniferous, decid_mixed, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -146,8 +151,8 @@ rmarkdown::render(
 #   (or let each part end with a comma)
 vars <- paste(
   "slope_no3_vs_time", 
-  "TOC",                                # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "TOC, altitude",                                # change here
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, total_forest, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -191,7 +196,7 @@ rmarkdown::render(
 vars <- paste(
   "slope_no3_vs_time", 
                                   # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, coniferous, decid_mixed, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -231,7 +236,7 @@ rmarkdown::render(
 vars <- paste(
   "slope_no3_vs_time", 
   # change here
-  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude, altitude",
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",
   "pre, tmp, slope_pre, slope_tmp, urban, cultivated, total_forest, total_shrub_herbaceous",
   "wetland, lake_water, bare_sparse",
   sep = ",")    # remember this
@@ -297,6 +302,50 @@ rmarkdown::render(
   output_file = '166d1_Time_series_results_James_no_altitude.html',                     # change here
   params = list(
     document_title = "166d1 Analyse NO3 decline - excl. altitude and catchment_area",                 # change here
+    text_dataset = "Dataset: all variables except altitude and catchment area (but including TOC)",  # change here
+    selected_vars = vars,
+    logistic_formula = form)
+)
+
+
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+#
+# 166d1x - As 166d1 but including slope_toc_vs_time (coniferous + decid_mixed separately) ----
+#
+#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o#o
+
+
+# If many parameters, split up the lines with a 'paste()'  
+# Remember to set that the parts should be joined using a comma (sep = ",") 
+#   (or let each part end with a comma)
+vars <- paste(
+  "slope_no3_vs_time", 
+  "TOC, slope_toc_vs_time",                                
+  "slope_dep_vs_time, NO3, TOTN_dep, latitude, longitude",   # altitude removed
+  "pre, tmp, slope_pre, slope_tmp, urban, cultivated, coniferous, decid_mixed, total_shrub_herbaceous",
+  "wetland, lake_water, bare_sparse",
+  sep = ",")    # remember this
+vars
+#
+# If long formula, split up the lines with a 'paste()'  
+#
+form <- paste(
+  "slope_no3_vs_time ~ TOC + slope_toc_vs_time +",                # change here
+  "TOTN_dep*slope_dep_vs_time + NO3 +", 
+  "decid_mixed + coniferous +",
+  "tmp + lake_water + wetland"
+)
+# form 
+
+# To find file names used:
+# dir(pattern = "166*")
+
+# Render HTML and .md files  
+rmarkdown::render(
+  input = '166parm_Time_series_results_James.Rmd',          
+  output_file = '166d1x_Time_series_results_James_no_altitude.html',                     # change here
+  params = list(
+    document_title = "166d1x Analyse NO3 decline - excl. altitude and catchment_area",                 # change here
     text_dataset = "Dataset: all variables except altitude and catchment area (but including TOC)",  # change here
     selected_vars = vars,
     logistic_formula = form)
